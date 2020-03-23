cd9e368c18deb7e8b0c066f6421ec83822909d86
umvahed <ahmedvahed@gmail.com>
2020-03-23 11:15:31 +0200

    Initial Upload
    First Commit

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double modLine = Math.Round(2.00 - 1.35, 2);
+            double mod1 = Math.Round(modLine % 0.25, 2); 
+            Console.WriteLine("modLine = " + modLine);
+            Console.WriteLine("mod1 = " + mod1);
+        }
+    }
+}
